ac18320f184c4752cfeccf20ec81dbf15150aaec
Ethan <ethananhphan@gmail.com>
2022-09-30 16:27:46 -0400

    Initial commit

---
+++ src/main/java/com/example/CS2340/Cs2340Application.java
@@ -0,0 +1,13 @@
+package com.example.CS2340;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2340Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2340Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CS2340/Cs2340ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CS2340;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2340ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
