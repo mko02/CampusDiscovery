dd5747b8eaf24b0d6235be85d6d87ecb4fd9db65
ephan8 <ephan8@gatech.edu>
2022-10-21 15:12:38 -0400

    Add CreateEvent component

---
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export function CreateEvent(){
+        return (
+            <div>
+                <h1>Create Event:</h1>
+                <p>Input boxes</p>
+            </div>
+        )
+
+}
--- src/components/exportPages.js
+++ src/components/exportPages.js
@@ -1,6 +1,7 @@
 export { Account } from "./Account/Account";
+export { CreateEvent } from "./CreateEvent/CreateEvent";
+export { Dashboard } from "./Dashboard/Dashboard";
 export { Header } from "./Header/Header";
 export { Home } from "./Home/Home";
-export { Dashboard } from "./Dashboard/Dashboard"
-export { Welcome } from "./WelcomePage/Welcome"
+export { Welcome } from "./WelcomePage/Welcome";
 
