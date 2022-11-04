09a930ea5a85f5d3c4faeb64fdad80d60a69e9c8
ezhou66 <ericzhou3@gmail.com>
2022-11-04 17:46:15 -0400

    fix minor bug

--- src/components/CreateEvent/CreateEvent.jsx
+++ src/components/CreateEvent/CreateEvent.jsx
@@ -109,7 +109,7 @@ export function CreateEvent() {
             timeStart,
             timeEnd,
             host,
-            inviteOnly
+            inviteOnly,
             capacity
           ).then((res) => {
             window.location.assign(`/#/event/dashboard`);
