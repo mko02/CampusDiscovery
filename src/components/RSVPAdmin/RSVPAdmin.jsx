c1315c94a6ccacb85a625bc49079f88f097d2c54
ezhou66 <ericzhou3@gmail.com>
2022-11-04 17:26:53 -0400

    displays user ids in distinct columns:

--- src/components/RSVPAdmin/RSVPAdmin.jsx
+++ src/components/RSVPAdmin/RSVPAdmin.jsx
@@ -7,12 +7,32 @@ import "./RSVPAdmin.css";
 export function RSVPAdmin() {
     const [inviteStatus, setInviteStatus] = useState(""); //0 is not invite-only, 1 is invite-only
     const [hasLoaded, setLoaded] = useState(false);
-    const [userID, setUserID] = useState("");
+    const [ will, setWill ] = useState([]);
+    const [ maybe, setMaybe ] = useState([]);
+    const [ wont, setWont ] = useState([]);
     const { id } = useParams();
 
     var willList = []
     var maybeList = []
     var wontList = []
+
+    const willListRender = will.map(function (item, index) {
+        return (
+            <div key={index}>{item}</div>
+        );
+    });
+
+    const maybeListRender = maybe.map(function (item, index) {
+        return (
+            <div key={index}>{item}</div>
+        );
+    });
+
+    const wontListRender = wont.map(function (item, index) {
+        return (
+            <div key={index}>{item}</div>
+        );
+    });
 
     function getAttendeeList() {
         if (!hasLoaded) {
@@ -22,32 +42,40 @@ export function RSVPAdmin() {
             getRSVP(id).then((snap) => {
                 const value = snap.val();
                 for (let user in value.users) {
-                    if (value.users[user].equals("Will Attend")) {
+                    let status = value.users[user].rsvpStatus
+                    if (status === "Will Attend") {
                         willList.push(user);
-                    } else if (value.users[user].equals("Maybe")) {
+                    } else if (status === "Maybe") {
                         maybeList.push(user);
                     } else {
                         wontList.push(user);
                     }
-                  }
+                }
+                setWill(willList);
+                setMaybe(maybeList);
+                setWont(wontList);
+                setLoaded(true);
             });
         }
     }
 
     useEffect(() => {
         getAttendeeList();
-      }, []);
+    }, []);
 
     return (
         <div className = "attendanceGrid">
             <div className = "column">
-                
+                <h1> Attending </h1>
+                {willListRender}
             </div>
             <div className = "column">
-                
+                <h1> Unsure </h1>
+                {maybeListRender}
             </div>
             <div className = "column">
-                
+                <h1> Not Attending </h1>
+                {wontListRender}
             </div> 
         </div>
     );
