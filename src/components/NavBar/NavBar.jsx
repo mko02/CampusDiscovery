491d84d7c3218448145f5c8ce4a3b8ed60cddb1d
ephan8 <ephan8@gatech.edu>
2022-11-28 17:46:59 -0500

    add account

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,6 +4,7 @@ import * as Styled from "./NavBar.styled";
 
 export function NavBar(){
     return (<Styled.Container>
+        <a href="/#/account"><Styled.Tab>Account</Styled.Tab></a>
         <a href="/#/dashboard"><Styled.Tab>Dashboard</Styled.Tab></a>
         <a href="/#/map"><Styled.Tab>Map</Styled.Tab></a>
         <a href="/#/filter"><Styled.Tab>Filter & Sort</Styled.Tab></a>
